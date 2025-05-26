70dc6645f47734422a8a9eaca538cdc0d79b03b8
prestigious-protagonist <jaskaranyt123@gmail.com>
2025-05-26 18:50:58 +0530

    production

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ app.use(bodyParser.urlencoded({extended: true}))
 const router = require('./routes/index')
 const { PORT } = require("./config/server-config")
 app.use('/orderService/api', router)
+app.get('/', (req, res) =>{
+    res.send("OrderService...")
+})
 app.listen(PORT, ()=>{
     console.log("Server listening on port 3006")
 })
